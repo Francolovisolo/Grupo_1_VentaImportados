aee9ac7b7fda5aafeb7d1adf7c35f42c56f99bdd
cesarnac@gmail.com <cesarnac@gmail.com>
2023-03-04 11:48:10 -0500

    Actualizando app.js

--- app.js
+++ app.js
@@ -15,18 +15,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/index.html"));
 });
 
-app.get("/register", (req, res) => {
+app.get("/login", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/login.html"));
 });
 
-app.get("/login", (req, res) => {
+app.get("/product", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/productCart.html"));
 });
 
-app.get("/login", (req, res) => {
+app.get("/detail", (req, res) => {
   res.sendFile(path.resolve(__dirname, "./views/productDetail.html"));
-});
-
-app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/register.html"));
 });
